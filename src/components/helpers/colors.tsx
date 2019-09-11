e2442ddf2a0f4ee88f01865523f07db022729eb1
Alexander Buyanov <alex.bu.89@gmail.com>
2019-09-11 18:06:29 +0200

    Add colors

--- src/components/helpers/colors.tsx
+++ src/components/helpers/colors.tsx
@@ -4,14 +4,27 @@ import { Component, Host, h, State } from '@stencil/core';
   tag: 'ab-colors',
   styleUrl: 'colors.scss',
 })
+
 export class Rating {
-  @State() colorsBig: string[];
-  @State() colorsSmall: string[];
+  @State() colorsBig: string[] = ['navi', 'blue', 'green', 'yellow', 'pink', 'cyan'];
+  @State() colorsSmall: string[] = [];
 
   render() {
     return  (
       <Host class="container">
-
+        <div class="row">
+          {
+            this.colorsBig.map((color) => {
+              return (
+                <div class={ `ab-color ${color}` }>
+                  <div class={`ab-circle big ${color}-b`}></div>
+                  <span class="ab-color-name">{ color }</span>
+                  <span class="ab-color-hash">#fff</span>
+                </div>
+              );
+            })
+          }
+        </div>
       </Host>
     );
   }
