3dd9827263f98e475c14ba7ed23c715c67dc1c21
Alex Buyanov <alex.bu.89@gmail.com>
2019-10-13 17:18:17 +0200

    Working on button component

---
+++ src/components/ab-button/ab-button.tsx
@@ -0,0 +1,31 @@
+import { Component, Host, Element, State, Prop, h } from '@stencil/core';
+
+@Component({
+  tag: 'ab-button',
+  styleUrl: 'ab-button.scss',
+  shadow: true
+})
+export class Rating {
+  @Prop() type: string;
+
+  @State() attribute = {
+    class: 'ab-button',
+    type: this.type || 'type="button"',
+    onClick: this.btnClick.bind(this),
+ };
+
+  @Element()
+  host: HTMLButtonElement;
+
+  btnClick() {
+    console.log("btnClick");
+  }
+
+  render() {
+    return  (
+      <Host>
+        <slot />
+      </Host>
+    );
+  }
+}
